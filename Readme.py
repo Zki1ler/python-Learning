b0bb544b1433ca44ac316f9c1f21cf84b3b05e12
NoahBradbury <NSBradbury01@indianatech.net>
2026-01-15 10:39:22 -0500

    Initial commit - first project

---
+++ Readme.py
@@ -0,0 +1,8 @@
+#My Python Learning Journey
+
+#This repository contains my python programming exercises and projects.
+
+## About Me
+## Name: Noah Bradbury
+## Course: CS1300
+## School: Indiana Tech
---
+++ test.py
@@ -0,0 +1,6 @@
+name = input("What is your name ")
+print("Hello, ", name, " Welcome to python")
+print("Python Version check:")
+import sys
+print(sys.version)
+
