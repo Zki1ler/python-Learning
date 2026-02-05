e333f2aa42a5cfa223802a7476116b346034b8a7
Noah Bradbury <152640324+Zki1ler@users.noreply.github.com>
2026-02-05 02:26:09 -0500

    Add files via upload

---
+++ HomeworkAssignment1.py
@@ -0,0 +1,7 @@
+first_name = "Noah"
+last_name = "Bradbury"
+examone = 98
+examtwo = 73
+examthree = 89
+average = (examone + examtwo + examthree) / 3
+print("Name:", first_name, last_name, "His Exam Average Is:", average)
